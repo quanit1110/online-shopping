88035f9ce1152f305d39697f482ccfed2fb72e43
Quan <quanpham1110@gmail.com>
2017-12-28 22:49:37 +0700

    commit lan hai

---
+++ shoppingbackend/src/main/java/quanpham/online/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package quanpham.online.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
